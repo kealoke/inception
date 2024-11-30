cc9f88d89aab8f05270d9978deba6867486892bd
kealoke <yuna.kealoke@gmail.com>
2024-11-30 21:52:06 +0900

    cant connect to wordpress

--- requirements/mariadb/script.sh
+++ requirements/mariadb/script.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
+# MariaDBデータディレクトリの初期化
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Initializing MariaDB data directory..."
+    mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+fi
+
+
 sed -e
 
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" > /var/lib/mysql/init.sql
-echo -e "create databese"
-echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_PWD';" >> /var/lib/mysql/init.sql
-echo -e "create user"
+echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PWD';" >> /var/lib/mysql/init.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%';" >> /var/lib/mysql/init.sql
-echo -e "add priv"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';" >> /var/lib/mysql/init.sql
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED USING PASSWORD('root');" >> /var/lib/mysql/init.sql
-echo -e $DB_ROOT_PWD
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PWD';" >> /var/lib/mysql/init.sql
 echo "FLUSH PRIVILEGES;" >> /var/lib/mysql/init.sql
-echo -e 'flush'
 #mysqlサービスを起動
-# mysqld --init-file=/var/lib/mysql/init.sql
-
-mysqld_safe &
+mysqld_safe --datadir=/var/lib/mysql &
 sleep 5
-
-mysql < /var/lib/mysql/init.sql
+mysql -uroot -p$DB_ROOT_PWD < /var/lib/mysql/init.sql
 
 # kill $(cat /var/run/mysqld/mysqld.pid)
-mysqladmin shutdown -uroot -proot
-
+mysqladmin shutdown -uroot -p$DB_ROOT_PWD
 
 # #mysqlデーモンをフォアグラウンドで起動させる
 exec mysqld_safe 
